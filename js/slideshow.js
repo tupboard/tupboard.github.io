7c286cd591da51cbdb2faef328902d654fec390c
Jason Brillon <Jason62513@gmail.com>
2014-11-22 12:04:26 -0500

    updated link… again

--- js/slideshow.js
+++ js/slideshow.js
@@ -3,7 +3,7 @@
       $('.left').on('click',displayPreviousImage);
       $('.right').on('click',displayNextImage);
       $('.b-left').on('click', function() {
-        window.location = "tupboard.github.io/aboutpboard.html";
+        window.location = "http://tupboard.github.io/aboutpboard.html";
       });
     });
 
