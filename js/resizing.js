ca4bedbd8cfc8ec0e444b34410962e1dd98ff54f
Jason Brillon <Jason62513@gmail.com>
2014-12-20 19:35:52 -0500

    removed slice

--- js/resizing.js
+++ js/resizing.js
@@ -1,12 +1,7 @@
 
 
     // Some buffer value, adjust this to get the rest of the content aligned properly
-$(document).ready(function() {
-  var windowHeight = $(window).height();
-  var percentage = $(window).height()/680;
-  var num = $('.scrollbox').css('height').slice(0,-2);
-  $('.scrollbox').css('height', num*percentage-10+'px');
-});
+
     $(document).ready(function() {
    var windowHeight = $(window).height();
    var $imageWrapper = $('.resizable');
