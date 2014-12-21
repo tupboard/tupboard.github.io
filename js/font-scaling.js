7c5975ec74f9f297be3ca5ac5305797816fe5f45
Jason Brillon <Jason62513@gmail.com>
2014-12-21 18:09:39 -0500

    limiting font to desktop

--- js/font-scaling.js
+++ js/font-scaling.js
@@ -1,4 +1,26 @@
+var isMobile = {
+    Android: function() {
+        return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function() {
+        return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: function() {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function() {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function() {
+        return navigator.userAgent.match(/IEMobile/i);
+    },
+    any: function() {
+        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+    }
+};
+
 $(document).ready(function() {
+if (!isMobile) {
    var windowHeight = $(window).height();
    var percentage = windowHeight / 680;
 
@@ -37,5 +59,5 @@ $(document).ready(function() {
    newsize = Math.floor(newsize*percentage);
    newsize = newsize+"px";
    $(".pxchange").css("font-size", newsize);
-
+}
    });
